ff5a856690843ffcc85ad52ccfbff9596d35d405
Colectivo de Trabajo en Ambiente y Salud (CoTAS) <grupolepto@gmail.com>
2026-06-19 14:15:29 -0300

    Add files via upload

--- KAP_dengue_sampling.R
+++ KAP_dengue_sampling.R
@@ -1,28 +1,41 @@
-### KAP dengue: estadísticas nacionales 2020 y cálculo tamaño muestral
-### Autora: Tamara Ricardo
-### Fecha modificación: # 2025-10-28 12:11:13
+### Data analysis for the manuscript: Unrecognized dengue transmission in socially
+### vulnerable periurban neighborhoods of a temperate Argentine city: integrating
+### serology with knowledge, attitudes and practices
+### Author: Tamara Ricardo
+# Última modificación: 19-06-2026 14:11
 
-# Cargar paquetes --------------------------------------------------------
+# Load packages ----------------------------------------------------------
 pacman::p_load(
+  epiR,
   rio,
   janitor,
-  epiR,
   tidyverse
 )
 
+# Sample size calculation ------------------------------------------------
+## Chalet -----
+epi.sssimpleestb(N = 277, epsilon = .2, Py = .5, se = 1, sp = 1)
 
-# Cargar datos MSAL 2020 --------------------------------------------------
-datos_msal_2020 <- import(
+## Colastiné Sur -----
+epi.sssimpleestb(N = 314, epsilon = .2, Py = .5, se = 1, sp = 1)
+
+
+## Vuelta del Paraguayo -----
+epi.sssimpleestb(N = 99, epsilon = .2, Py = .5, se = 1, sp = 1)
+
+
+# Dengue incidence -------------------------------------------------------
+## Load/clean dataset -----
+data_msal_2020 <- import(
   "raw/informacion-publica-dengue-zika-nacional-hasta-20201231_1.xlsx"
 ) |>
-
-  # Descartar columnas innecesarias
+  ## Remove id columns
   select(-ends_with("_id")) |>
 
-  # Seleccionar casos de Santa Fe
+  ### Select cases from Santa Fe
   filter(provincia_nombre == "Santa Fe") |>
 
-  # Definir NAs
+  # Clean NAs
   mutate(across(
     .cols = where(is.character),
     .fns = ~ if_else(
@@ -32,42 +45,23 @@ datos_msal_2020 <- import(
     )
   )) |>
 
-  # Agrupar por semana epidemiológica
+  ### Group data
   count(año, semanas_epidemiologicas, wt = cantidad_casos)
 
 
-## Análisis exploratorio
-# Total casos dengue Santa Fe semanas 1-52 año 2020
-sum(datos_msal_2020$n)
+## Cases of dengue in Santa Fe (2020) -----
+sum(data_msal_2020$n)
 
-# Incidencia Santa Fe 2020
-attack_rate(
-  cases = sum(datos_msal_2020$n),
-  population = 572265,
-  multiplier = 10^4,
-  mergeCI = T,
-  digits = 1
+
+# Incidence of dengue in Santa Fe (2020) -----
+epi.prev(
+  pos = sum(data_msal_2020$n),
+  tested = 572265,
+  sp = 1,
+  se = 1,
+  units = 1e4
 )
 
-# Limpia environment
+
+# Clean working environment ----------------------------------------------
 rm(list = ls())
-
-
-# Calcular tamaño muestral -----------------------------------------------
-## Chalet
-epi.sssimpleestb(N = 277, epsilon = .2, Py = .5, se = 1, sp = 1)
-
-# Ajustar x sensibilidad y especificidad test
-epi.sssimpleestb(N = 277, epsilon = .2, Py = .5, se = .96, sp = .93)
-
-## Colastiné Sur
-epi.sssimpleestb(N = 314, epsilon = .2, Py = .5, se = 1, sp = 1)
-
-# Ajustar x sensibilidad y especificidad test
-epi.sssimpleestb(N = 314, epsilon = .2, Py = .5, se = .96, sp = .93)
-
-## Vuelta del Paraguayo
-epi.sssimpleestb(N = 99, epsilon = .2, Py = .5, se = 1, sp = 1)
-
-# Ajustar x sensibilidad y especificidad test
-epi.sssimpleestb(N = 99, epsilon = .2, Py = .5, se = .96, sp = .93)
